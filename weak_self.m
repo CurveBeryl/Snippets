6a13137659e20c6d5884c6819d45d5a9db27137e
Juan Jose Karam <JuanKaram@Juans-MacBook-Pro.local>
2014-03-20 13:38:42 -0700

    Imported user code snippets

---
+++ weak_self.m
@@ -0,0 +1,10 @@
+// Weak Self
+// Declares a weak instance of the current instance
+//
+// IDECodeSnippetCompletionPrefix: weakself
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: AF1D57E3-7DE1-400A-BBD9-CE7D66EECF11
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+__weak typeof(self)weakSelf = self;
